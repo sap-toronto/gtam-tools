8e89b9542835396e1320b184328afcfe83b8f496
Brian Cheung <brian.cheung@wsp.com>
2021-02-17 19:52:17 -0500

    Enhanced `read_tts_cross_tabulation_file()` to be automatically flexible in handling TTS cross tabulation data in table or column formats

--- gtam_tools/utils.py
+++ gtam_tools/utils.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 import pandas as pd
 from pathlib import Path
+import re
 from shapely.wkb import loads, dumps
 from typing import Union
 
@@ -24,12 +25,11 @@ def format_gdf(gdf: gpd.GeoDataFrame, index_col: str = None) -> gpd.GeoDataFrame
     return gdf
 
 
-def read_tts_cross_tabulation_file(fp: Union[str, Path], column_format: bool = False) -> pd.DataFrame:
+def read_tts_cross_tabulation_file(fp: Union[str, Path]) -> pd.DataFrame:
     """A function to read a TTS Cross Tabulation file downloaded from the DMG Data Retrieval System.
 
     Args:
         fp (Union[str, Path]): File path to the TTS Cross Tabulation file.
-        column_format (bool, optional): Defaults to ``False``. Identifies if the file is in column format.
 
     Returns:
         pd.DataFrame
@@ -39,42 +39,57 @@ def read_tts_cross_tabulation_file(fp: Union[str, Path], column_format: bool = F
 
     # Determine query properties
     row_att = None
+    col_att = None
     table_att = None
     table_headings = []
     with open(fp) as f:
         for i, line in enumerate(f, start=1):
             if row_att is None and 'Row:' in line:  # only find the first instance
                 row_att = line.split('-')[-1].strip()
-            if table_att is None and 'Table:' in line:  # only find the first instance
-                table_att = line.split('-')[-1].strip()
-            if 'TABLE' in line:
-                table_headings.append((i, line.split(':')[-1].replace(table_att, '').strip()[1:-1]))
+            if col_att is None and 'Column:' in line:  # only find the first instance
+                col_att = line.split('-')[-1].strip()
+            if 'Table:' in line or 'TABLE' in line:
+                if table_att is None:  # only for the first instance
+                    table_att = line.split('-')[-1].strip()
+                else:
+                    table_name = re.sub('[()]', '', line.split(':')[-1].replace(table_att, '').strip())
+                    table_headings.append((i, table_name))
         n_lines = i + 1
 
-    if column_format:
-        # Read data from file
-        if table_att is not None and len(table_headings) > 0:
-            table = []
-            for i in range(0, len(table_headings)):
-                start_row, table_name = table_headings[i]
-                end_row = table_headings[i + 1][0] if i < len(table_headings) - 1 else n_lines + 1
+    # Read data from file
+    if len(table_headings) > 0:
+        table = []
+        for i in range(0, len(table_headings)):
+            start_row, table_name = table_headings[i]
+            end_row = table_headings[i + 1][0] if i < len(table_headings) - 1 else n_lines + 1
 
-                skip_rows = start_row + 1
-                n_rows = end_row - start_row - 4
+            skip_rows = start_row + 1
+            n_rows = end_row - start_row - 4
 
-                df = pd.read_csv(fp, skipinitialspace=True, skiprows=skip_rows, nrows=n_rows, delim_whitespace=True)
-                df[table_att] = table_name
-                table.append(df)
-            table = pd.concat(table, axis=0, ignore_index=True)
-        else:
-            header_row = None
-            with open(fp) as f:
-                for i, line in enumerate(f, start=1):
-                    if line.strip().startswith(row_att):
-                        header_row = i
-                        break
-            table = pd.read_csv(fp, skipinitialspace=True, skiprows=header_row - 1, delim_whitespace=True)
+            df = _read_csv_tts_ct_data(fp, row_att, col_att, skip_rows, nrows=n_rows, skipinitialspace=True)
+            df[table_att] = table_name
+            table.append(df)
+        table = pd.concat(table, axis=0, ignore_index=True)
     else:
-        raise NotImplementedError
+        header_row = None
+        with open(fp) as f:
+            for i, line in enumerate(f, start=1):
+                if line.strip().startswith(row_att) or line.strip().startswith(','):
+                    header_row = i
+                    break
+        df = _read_csv_tts_ct_data(fp, row_att, col_att, skip_rows=header_row - 1, skipinitialspace=True)
 
     return table
+
+
+def _read_csv_tts_ct_data(fp: Path, row_att: str, col_att: str, skip_rows: int, **kwargs) -> pd.DataFrame:
+    try:  # First try column format
+        df = pd.read_csv(fp, index_col=[row_att, col_att], skiprows=skip_rows, delim_whitespace=True, **kwargs)
+    except ValueError:  # Else try table format
+        df = pd.read_csv(fp, index_col=0, skiprows=skip_rows, **kwargs)
+        df.index.name = row_att
+        df.columns.name = col_att
+        df = df.stack().to_frame(name='total')
+    df.reset_index(inplace=True)
+
+    return df
