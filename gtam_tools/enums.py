b1dff2e74edde814e97fac979677f4eecff58065
Brian Cheung <bccheung@outlook.com>
2021-03-29 16:10:05 -0400

    Extended enums functionality

--- gtam_tools/enums.py
+++ gtam_tools/enums.py
@@ -4,14 +4,39 @@ from enum import Enum, IntEnum
 _PHF = namedtuple('PHF', ['AUTO', 'LIGHT', 'MEDIUM', 'HEAVY'])
 
 
-class CordonCountDir(IntEnum):
+class _BaseEnum(Enum):  # Extending functionality of the Enum class a bit
+    @classmethod
+    def as_dict(cls):
+        return {k: v.value for k, v in cls.__members__.items()}
+
+    def __eq__(self, other):  # For comparisons (e.g. PCEFactors.MEDIUM == 1.75 returns True)
+        return self.value == other
+
+    def __get__(self, obj, type=None):  # For retrieving values (eg.g var = PCEFactors.MEDIUM ==> var = 1.75)
+        return self.value
+
+
+class _BaseIntEnum(IntEnum):  # Extending functionality of the IntEnum class a bit
+    @classmethod
+    def as_dict(cls):
+        return {k: v.value for k, v in cls.__members__.items()}
+
+
+class CordonCountDir(_BaseIntEnum):
     NORTH = 1
     SOUTH = 3
     EAST = 2
     WEST = 4
 
 
-class PCEFactors(Enum):
+class OccupationCategories(_BaseEnum):
+    G = 'General Office/Clerical'
+    M = 'Manufacturing/Construction/Trades'
+    P = 'Professional/Management/Technical'
+    S = 'Retail Sales and Service'
+
+
+class PCEFactors(_BaseEnum):
     MEDIUM = 1.75
     HEAVY = 2.5
 
@@ -24,11 +49,17 @@ class PHFactors(Enum):
     EV = _PHF(0.2, 0.2, 0.2, 0.2)
 
 
-class TimeFormat(Enum):
+class StudentCategories(_BaseEnum):
+    P = 'Primary'
+    S = 'Secondary'
+    U = 'Post-Secondary'
+
+
+class TimeFormat(_BaseEnum):
     MINUTE_DELTA = 'minute_delta'
     COLON_SEP = 'colon_separated'
 
 
-class ZoneNums(IntEnum):
+class ZoneNums(_BaseIntEnum):
     MAX_INTERNAL = 6000
     ROAMING = 8888
