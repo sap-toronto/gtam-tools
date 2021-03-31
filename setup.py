1798da325168059fe42985df6b769946c901f138
Brian Cheung <bccheung@outlook.com>
2021-03-31 15:50:44 -0400

    Integrated conda build bat script command into meta.yaml

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='A Python package for handling GTAModel data',
     url='https://github.com/sap-toronto/gtam-tools',
     author='WSP',
-    maintatiner='Brian Cheung',
+    maintainer='Brian Cheung',
     maintainer_email='brian.cheung@wsp.com',
     classifiers=[
         'License :: OSI Approved :: MIT License'
