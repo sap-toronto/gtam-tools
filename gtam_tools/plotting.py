5e91a4abc75faa53d3a53a20d25a7609e2dfe3cf
Brian Cheung <bccheung@outlook.com>
2021-04-23 10:45:27 -0400

    Added `stacked_hbar_comparison()` plotting function

--- gtam_tools/plotting.py
+++ gtam_tools/plotting.py
@@ -1,7 +1,7 @@
 from bokeh.plotting import Figure, figure
-from bokeh.models import CDSView, ColumnDataSource, Div, GroupFilter, Slope
+from bokeh.models import CDSView, ColumnDataSource, Div, FactorRange, GroupFilter, NumeralTickFormatter, Slope
 from bokeh.layouts import Column, column, GridBox, gridplot
-from bokeh.palettes import Category20
+from bokeh.palettes import Category20, Set3
 import numpy as np
 import pandas as pd
 from typing import Any, Dict, List, Tuple, Union, Hashable
@@ -211,6 +211,144 @@ def scatterplot_comparison(controls_df: pd.DataFrame, result_df: pd.DataFrame, d
     if calc_pct_diff:
         df['pct_diff'] = (df[result_name] - df[controls_name]) / df[controls_name] * 100
         df['pct_diff'] = df['pct_diff'].replace([np.inf, -np.inf], np.nan)
+
+    return df, fig
+
+
+def stacked_hbar_comparison(controls_df: pd.DataFrame, result_df: pd.DataFrame, data_label: str, *,
+                            ref_label: Union[str, List[str]] = None,  category_labels: Dict = None,
+                            label_col: Union[str, List[str]] = None, controls_name: str = 'controls',
+                            result_name: str = 'model', x_axis_label: str = None, figure_title: str = None,
+                            plot_height: int = None, normalize: bool = True, color_palette: Dict[int, Any] = Set3
+                            ) -> Tuple[pd.DataFrame, Union[Column, Figure, GridBox]]:
+    """Creates an interactive Bokeh-based stacked horizontal bar chart to compare data
+
+    Args:
+        controls_df (pd.DataFrame): A DataFrame containing control values. Must be in wide-format where rows represent
+            a reference (e.g. count station, TAZ, geography, etc.) and columns represent the data categories.
+        result_df (pd.DataFrame): A DataFrame containing modelled values. Uses the same format as `controls_df`.
+        data_label (str): The name to use for the data represented by the `controls_df` and `result_df` columns.
+        ref_label (Union[str, List[str]], optional): Defaults to ``None``. The name(s) corresponding to the
+            ``controls_df`` and ``result_df`` indices. The function will try to infer the name(s) from indices of the
+            source DataFrames. If the indicies of the DataFrames are not set, then values must be set for this
+            parameter, otherwise an error will be raised. If providing a value to this parameter and the indices of the
+            source DataFrames are MultiIndex objects, then the provided value must be a list of strings.
+        category_labels (Dict, optional): Defaults to ``None``. Category labels used to rename the `controls_df` and
+            `result_df` columns.
+        label_col (Union[str, List[str]], optional): Defaults to ``None``. The columns to use when for figure axis
+            grouping.
+        controls_name (str, optional): Defaults to ``'controls'``. The name for the controls.
+        result_name (str, optional): Defaults to ``'model'``. The name for the results.
+        x_axis_label (str, optional): Defaults to ``None``. The label to apply to the x axis
+        figure_title (str, optional): Defaults to ``None``. The chart title to use.
+        plot_height (int, optional): Defaults to ``None``. The desired plot height. For facet plots, this value will be
+            set for each subplot.
+        normalize (bool, optional): Defaults to ``True``. Plot the stacked horizontal bar chart with normalized data.
+        color_palette (Dict[str, Any], optional): Defaults to ``Set3``. The Bokeh color palette to use.
+
+    Returns:
+        Tuple[pd.DataFrame, Union[Column, Figure, GridBox]]
+    """
+
+    if not controls_df.index.equals(result_df.index):
+        warnings.warn('Indices for `controls_df` and `result_df` are not identical; function may not produce desired '
+                      'results')
+    if not controls_df.columns.equals(result_df.columns):
+        warnings.warn('Columns for `controls_df` and `result_df` are not identical; function may not produce desired '
+                      'results')
+
+    if ref_label is None:
+        assert np.all(controls_df.index.names == result_df.index.names), 'Unable to resolve different index names, ' \
+                                                                         'please specify values for `ref_label` instead'
+        assert not (None in controls_df.index.names), 'Some index levels in `controls_df` do not have names'
+        assert not (None in result_df.index.names), 'Some index levels in `result_df` do not have names'
+        ref_label = list(controls_df.index.names)
+    elif isinstance(ref_label, Hashable):
+        ref_label = [ref_label]
+    elif isinstance(ref_label, List):
+        pass
+    else:
+        raise RuntimeError('Invalid data type provided for `ref_label`')
+
+    if label_col is None:
+        label_col = controls_df.index.names
+    elif isinstance(label_col, Hashable):
+        label_col = [label_col]
+    elif isinstance(label_col, List):
+        pass
+    else:
+        raise RuntimeError('Invalid data type provided for `label_col`')
+
+    # Prepare data
+    df = controls_df.copy()
+    df.columns.name = data_label
+    df = df.stack().to_frame(name=controls_name)
+    if ref_label is not None:
+        df.index.names = [*ref_label, data_label]
+
+    df[result_name] = result_df.stack()
+    df = df.fillna(0).round(0).astype(np.int64)
+    df.columns.name = 'source'
+    df = df.stack().to_frame(name='trips').reset_index()
+
+    if category_labels is not None:
+        df[data_label] = df[data_label].map(category_labels)
+
+    # Prepare figure-specific data
+    fig_df = df.copy()
+    fig_df['label_col'] = ''
+    for i, col in enumerate(label_col):
+        if i == 0:
+            fig_df['label_col'] += fig_df[col].astype(str)
+        else:
+            fig_df['label_col'] += ' - ' + fig_df[col].astype(str)
+    fig_df = fig_df.pivot_table(values='trips', index=['label_col', 'source'], columns=data_label, aggfunc='sum',
+                                fill_value=0)
+    fig_df.sort_index(ascending=False, inplace=True)
+
+    if normalize:
+        fig_df = (fig_df / fig_df.sum(axis=1).to_numpy()[:, np.newaxis]).round(3)
+
+    # Plot figure
+    x_range = (0, 1) if normalize else (0, int(fig_df.sum(axis=1).max()))
+    tooltips = '$name (@y) = @$name{0.0%}' if normalize else '$name (@y) = @$name'
+    n_colors = max(len(df[data_label].unique()), 3)
+
+    figure_params = {
+        'toolbar_location': 'above', 'sizing_mode': 'stretch_both', 'tools': 'xpan,xwheel_zoom,hover,save,reset',
+        'output_backend': 'webgl'
+    }
+    if plot_height is not None:
+        figure_params['plot_height'] = plot_height
+    if x_axis_label is not None:
+        figure_params['x_axis_label'] = x_axis_label
+
+    source = fig_df.to_dict(orient='list')
+    source['y'] = fig_df.index.tolist()
+    source = ColumnDataSource(source)
+
+    factors = fig_df.index.tolist()
+    columns = fig_df.columns.tolist()
+
+    fig = figure(x_range=x_range, y_range=FactorRange(*factors), tooltips=tooltips, **figure_params)
+
+    fig.hbar_stack(columns, y='y', source=source, color=color_palette[n_colors], legend_label=columns)
+
+    if normalize:
+        fig.xaxis.formatter = NumeralTickFormatter(format='0%')
+
+    fig.y_range.factor_padding = 0.25
+    fig.y_range.group_padding = 1
+
+    fig.yaxis.group_label_orientation = 'horizontal'
+
+    fig.add_layout(fig.legend[0], 'above')
+    fig.legend.margin = 20
+    fig.legend.orientation = 'horizontal'
+    fig.legend.spacing = 5
+
+    if figure_title is not None:
+        fig = _wrap_figure_title(fig, figure_title)
 
     return df, fig
 
