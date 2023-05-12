873e40a4b8a5daaf7477073529f2f3c5c0f78e93
Brian Cheung <bccheung@outlook.com>
2023-05-12 15:04:20 -0400

    Refactored `save_bokeh_figure`

---
+++ gtam_tools/plotting/io.py
@@ -0,0 +1,30 @@
+from os import PathLike
+from pathlib import Path
+from typing import Union
+
+from bokeh.plotting import output_file, save
+from bokeh.resources import ResourcesMode
+
+
+def save_bokeh_figure(fig, dst: Union[str, PathLike], *, title: str = None, mode: ResourcesMode = None,
+                      root_dir: PathLike = None) -> None:
+    """Saves a Bokeh figure to an HTML document
+
+    Args:
+        fig: The Bokeh figure object to save.
+
+        dst (str | PathLike): The destination filepath to save figure to.
+
+        title (str, optional): Defaults to ``None``. A title for the HTML document.
+
+        mode (str, optional) : Defaults to ``'cdn'``. how to include BokehJS.
+            One of: ``'inline'``, ``'cdn'``, ``'relative(-dev)'`` or
+            ``'absolute(-dev)'``. See `bokeh.resources.Resources` for more details.
+
+        root_dir (str, optional): Defaults to ``None``. The root directory to use for 'absolute' resources.
+            This value is ignored for other resource types, e.g. ``INLINE`` or ``CDN``.
+    """
+    dst = Path(dst)
+    title = dst.stem if title is None else title
+    output_file(dst, title=title, mode=mode, root_dir=root_dir)
+    save(fig)
