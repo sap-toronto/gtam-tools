f0ac037fbb4b7708fd13806b5e858d1661c362a9
jkmende05 <156609804+jkmende05@users.noreply.github.com>
2024-05-21 10:28:54 -0400

    Refactored scatterplot function

--- gtam_tools/plotting/io.py
+++ gtam_tools/plotting/io.py
@@ -4,25 +4,21 @@ from os import PathLike
 from pathlib import Path
 from typing import Union
 
+from bokeh.layouts import Column, GridBox
+from bokeh.plotting import figure
+from bokeh.models import Tabs
+
 from bokeh.plotting import output_file, save
-from bokeh.resources import ResourcesMode
 
-
-def save_bokeh_figure(fig, dst: Union[str, PathLike], *, title: str = None, mode: ResourcesMode = None,
-                      root_dir: Union[str, PathLike] = None) -> None:
-    """Saves a Bokeh figure to an HTML document
+def save_bokeh_figure(fig: Union[Column, figure, GridBox, Tabs], dst: Union[str, PathLike], *, title: str = None):
+    """Saves a bokeh figure to an HTML documnet
 
     Args:
-        fig: The Bokeh figure object to save.
-        dst (str | PathLike): The destination filepath to save figure to.
-        title (str, optional): Defaults to ``None``. A title for the HTML document.
-        mode (str, optional) : Defaults to ``'cdn'``. how to include BokehJS.
-            One of: ``'inline'``, ``'cdn'``, ``'relative(-dev)'`` or
-            ``'absolute(-dev)'``. See `bokeh.resources.Resources` for more details.
-        root_dir (str | PathLike, optional): Defaults to ``None``. The root directory to use for 'absolute' resources.
-            This value is ignored for other resource types, e.g. ``INLINE`` or ``CDN``.
+        fig: The bokeh figure object to save
+        dst (str | Pathlike): The destination filepath to save the figure object to.
+        title (str, optional): Defaults to ''None''. A title for the HTML document
     """
     dst = Path(dst)
     title = dst.stem if title is None else title
-    output_file(dst, title=title, mode=mode, root_dir=root_dir)
+    output_file(dst.as_posix(), title=title, mode='cdn')
     save(fig)
