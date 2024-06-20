ce51f473e67e576559c565427b90a2a6c70cfd8e
jkmende05 <156609804+jkmende05@users.noreply.github.com>
2024-06-20 11:21:22 -0400

    Refactored tlfd.py

--- gtam_tools/plotting/tlfd.py
+++ gtam_tools/plotting/tlfd.py
@@ -9,8 +9,7 @@ from bokeh.layouts import gridplot
 from bokeh.models import ColumnDataSource, TabPanel, Tabs
 from bokeh.plotting import figure
 
-from .common import check_df_indices, prep_figure_params, wrap_title
-from .resources import SyncAxesOptions
+from common import _check_df_indices, _prep_figure_params, _wrap_figure_title
 
 
 def _core_simplify_tlfd_index(df: Union[pd.DataFrame, pd.Series], low: float = -2.0,
@@ -47,32 +46,12 @@ def _core_prep_tlfd_data(controls_df: pd.DataFrame, result_df: pd.DataFrame, dat
     return df
 
 
-def _core_create_tlfd_plot(df: pd.DataFrame, controls_name: str, result_name: str, figure_params: Dict[str, Any], *,
-                           legend_label_text_font_size: str = '11px', controls_line_colour: str = 'red',
-                           controls_line_width: int = 2, **kwargs) -> figure:
-    source = ColumnDataSource(df)
-
-    p: figure = figure(**figure_params, **kwargs)
-    p.line(
-        source=source, x='bin_start', y='target', line_color=controls_line_colour, line_width=controls_line_width,
-        legend_label=controls_name
-    )
-    p.vbar(
-        source=source, x='bin_start', top='model', bottom=0, width=1.25, legend_label=result_name, fill_alpha=0.2
-    )
-    p.y_range.start = 0
-    p.legend.label_text_font_size = legend_label_text_font_size
-
-    return p
-
-
 def tlfd_facet_plot(controls_df: pd.DataFrame, result_df: pd.DataFrame, data_label: str, *,
                     category_labels: Dict = None, controls_name: str = 'controls', result_name: str = 'model',
                     bin_start: int = 0, bin_end: int = 200, bin_step: int = 2, facet_col_wrap: int = 2,
-                    facet_sort_order: bool = True, facet_sync_axes: SyncAxesOptions = 'both',
+                    facet_sort_order: bool = True, facet_sync_axes: str = 'both',
                     facet_max_subplot: int = 9, legend_label_text_font_size: str = '11px', figure_title: str = None,
-                    plot_width: int = None, plot_height: int = None, sizing_mode: SizingModeType = None,
-                    controls_line_colour: str = 'red', controls_line_width: int = 2):
+                    plot_height: int = None, controls_line_colour: str = 'red', controls_line_width: int = 2):
     """Create an interactive Bokeh-based facet plot of TLFD diagrams using the trips table from a MicrosimData instance
     and a targets table.
 
@@ -97,12 +76,8 @@ def tlfd_facet_plot(controls_df: pd.DataFrame, result_df: pd.DataFrame, data_lab
             number of subplots exceed this value, a tabbed interface will be used.
         legend_label_text_font_size (str, optional): Defaults to ``'11px'``. The text size of the legend labels.
         figure_title (str, optional): Defaults to ``None``. The chart title to use.
-        plot_width (int, optional): Defaults to ``None``. The desired plot width. For facet plots, this value will be
-            set for each subplot.
         plot_height (int, optional): Defaults to ``None``. The desired plot height. For facet plots, this value will be
             set for each subplot.
-        sizing_mode (SizingModeType, optional): Defaults to ``None``. A Bokeh SizingModeType. How will the items in the
-            layout resize to fill the available space. Please refer to Bokeh documentation for acceptable values.
         controls_line_colour (str, optional): Defaults to ``'red'``. The colour to use for the control target lines.
             Accepts html colour names.
         controls_line_width (int, optional): Defaults to ``2``. The line width to use for the control target lines.
@@ -111,7 +86,7 @@ def tlfd_facet_plot(controls_df: pd.DataFrame, result_df: pd.DataFrame, data_lab
         A pandas DataFrame and a Bokeh figure
     """
 
-    check_df_indices(controls_df, result_df)
+    _check_df_indices(controls_df, result_df)
 
     df = _core_prep_tlfd_data(
         controls_df, result_df, data_label, category_labels=category_labels, bin_start=bin_start, bin_end=bin_end,
@@ -120,7 +95,7 @@ def tlfd_facet_plot(controls_df: pd.DataFrame, result_df: pd.DataFrame, data_lab
 
     # Prepare figure formatting values
     tooltips = [('bin_start', '@bin_start'), (result_name, '@model{0.3f}'), (controls_name, '@target{0.3f}')]
-    figure_params = prep_figure_params('Bin', 'Proportion', tooltips, plot_width=plot_width, plot_height=plot_height)
+    figure_params = _prep_figure_params('Bin', 'Proportion', tooltips, plot_height=plot_height)
 
     # Determine number of facet plots and if groupings are needed
     n = facet_max_subplot
@@ -130,34 +105,49 @@ def tlfd_facet_plot(controls_df: pd.DataFrame, result_df: pd.DataFrame, data_lab
 
     plots = []
     for i, fc_items in enumerate(facet_col_items):
-        subplots = []
+        # Create plots
+        fig = []
         linked_axes = {}
         for j, fc in enumerate(fc_items):
+            p = figure(title=fc, **figure_params, **linked_axes)
             subset = df[df[data_label] == fc]  # We do it this way because CDSView doesn't support connected lines
-            p = _core_create_tlfd_plot(
-                subset, controls_name, result_name, figure_params,
-                legend_label_text_font_size=legend_label_text_font_size, controls_line_colour=controls_line_colour,
-                controls_line_width=controls_line_width, title=fc, **linked_axes
-            )
+            source = ColumnDataSource(subset)
+            p.line(source=source, x='bin_start', y='target', line_color=controls_line_colour,
+                   line_width=controls_line_width, legend_label=controls_name)
+            p.vbar(source=source, x='bin_start', top='model', bottom=0, width=1.25, legend_label=result_name,
+                   fill_alpha=0.2)
+            p.y_range.start = 0
+            p.legend.label_text_font_size = legend_label_text_font_size
+
             if (j == 0) and (facet_sync_axes is not None):
                 if facet_sync_axes.lower() in ['x', 'both']:
                     linked_axes['x_range'] = p.x_range
                 if facet_sync_axes.lower() in ['y', 'both']:
                     linked_axes['y_range'] = p.y_range
-            subplots.append(p)
-        plot = gridplot(
-            subplots, ncols=facet_col_wrap, merge_tools=True,
-            sizing_mode='stretch_both' if figure_title is None else sizing_mode
-        )
-        if len(facet_col_items) > 1:  # If there will be multiple tabs, convert figure into a TabPanel
+
+            fig.append(p)
+
+        if plot_height is None:
+            fig = gridplot(fig, ncols=facet_col_wrap, sizing_mode='stretch_both', merge_tools=True)
+        else:
+            fig = gridplot(fig, ncols=facet_col_wrap, sizing_mode='stretch_width', merge_tools=True, height=plot_height)
+        
+        if len(facet_col_items) > 1:  # If there will be multiple tabs, convert figure into a Panel
             start_num = i * n + 1
             end_num = i * n + len(fc_items)
-            plot = TabPanel(child=plot, title=f'Plots {start_num}-{end_num}')
-        plots.append(plot)
+            plots.append(TabPanel(child=fig, title=f'Plots {start_num}-{end_num}'))
+        else:
+            plots.append(fig)
 
-    fig = plots[0] if len(plots) == 1 else Tabs(tabs=plots)
+    if len(plots) == 1:
+        fig = plots[0]
+    else:
+        if plot_height is None:
+            fig = Tabs(tabs=plots, sizing_mode = 'stretch_both')
+        else:
+            fig = Tabs(tabs = plots, sizing_mode = 'stretch_width', height = plot_height)
 
     if figure_title is not None:
-        fig = wrap_title(fig, figure_title, sizing_mode=sizing_mode)
+        fig = _wrap_figure_title(fig, figure_title)
 
     return df, fig
